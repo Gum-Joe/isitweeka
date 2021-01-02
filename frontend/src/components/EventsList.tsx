1ca63ca4c59b622250802473ab563fde0e4e5201
Kishan <kishansambhi@hotmail.co.uk>
2021-01-02 18:23:52 +0000

    Move to an isEventType field

--- frontend/src/components/EventsList.tsx
+++ frontend/src/components/EventsList.tsx
@@ -1,11 +1,21 @@
+
+/**
+ * Used to distinguish between the different type of event we can display.
+ */
+export enum EventTypes{
+	HOUSE = "house", // house event
+	CHARITY = "charity", // charity event
+} 
+
 export interface EventItem {
 	title: string;
+	// HACK: So that it won't complain about the JSON. Please remove eventually
+	eventType: EventTypes | string;
 	ticketsURL: string;
 	ticketsSale: {
 		start: string;
 		end?: string;
 	};
-	isHouseEvent?: boolean;
 	headerURL: string;
 	backgroundColor: string;
 }
@@ -19,12 +29,13 @@ export const dummyResponse: EventData = {
 	events: [
 		{
 			title: "Would I Lie To You?",
-			ticketsURL: "#",
+			ticketsURL: "https://www.eventbrite.co.uk/e/would-i-lie-to-you-students-vs-teachers-tickets-133890123965?aff=isitweeka",
 			headerURL: "/Logo_Export_Trans_but_not_on_HRT.png",
 			backgroundColor: "#2C1F39",
 			ticketsSale: {
 				start: "1/1/2020",
 			},
+			eventType: "charity"
 		}
 	],
 	generatedAt: new Date().toISOString(),
