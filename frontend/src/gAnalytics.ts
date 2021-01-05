7d1922bf94444cb74f116c68361aae14561be3ff
Kishan <kishansambhi@hotmail.co.uk>
2021-01-05 19:05:12 +0000

    Lint fixes

--- frontend/src/gAnalytics.ts
+++ frontend/src/gAnalytics.ts
@@ -1,8 +1,8 @@
 // Set to the same value as the web property used on the site
-export const GA_PROPERTY = 'G-X4VMSWGN74';
+export const GA_PROPERTY = "G-X4VMSWGN74";
 
 // Disable tracking if the opt-out cookie exists.
-export const GA_DISABLE_COOKIE_STR = 'ga-disable-' + GA_PROPERTY;
+export const GA_DISABLE_COOKIE_STR = "ga-disable-" + GA_PROPERTY;
 /*if (document.cookie.indexOf(disableStr + '=true') > -1) {
 	// @ts-ignore
 	window[disableStr] = true;
@@ -12,6 +12,7 @@ export const GA_DISABLE_COOKIE_STR = 'ga-disable-' + GA_PROPERTY;
 /** Use true to opt out */
 export default function gaSetState(state: boolean): void {
 	document.cookie = GA_DISABLE_COOKIE_STR + `=${state ? "true" : "false"}; expires=Thu, 31 Dec 2099 23:59:59 UTC; path=/`;
+	// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 	// @ts-ignore
 	window[GA_DISABLE_COOKIE_STR] = state;
 }
