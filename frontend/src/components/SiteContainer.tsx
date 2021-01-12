50a3567a483707e8982973bde48e51c09e5547aa
Kishan <kishansambhi@hotmail.co.uk>
2021-01-12 17:37:07 +0000

    REMOVE the API Key (will revoke)

--- frontend/src/components/SiteContainer.tsx
+++ frontend/src/components/SiteContainer.tsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import EventsList, { EventData } from "./EventsList";
 import Button from "./Button.Forward";
 import dummyResponse from "../events.json";
-import { API_KEY, GregorianDay } from "../utils/constants";
+import { GregorianDay } from "../utils/constants";
 import { scrollUp, scrollDown } from "../utils/scroll";
 import * as ical from "ical";
 
