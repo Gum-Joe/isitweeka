29282eace73e6127370db0e46b743f0e0e1ad48f
Madeline Hart <madeline@rykantech.com>
2021-01-14 12:37:36 +0000

    hide back button on mobile (possibly temp, alternative in the works)

--- frontend/src/components/EventsList.tsx
+++ frontend/src/components/EventsList.tsx
@@ -116,7 +116,7 @@ export default class EventsList extends Component<EventsListProps> {
 	render() {
 		return (
 			<div className="isitweeka events">
-				<h2><button onClick={scrollUp} className="back" /> Upcoming Events</h2>
+				<h2 className="desktop"><button onClick={scrollUp} className="back" /> Upcoming Events</h2>
 				<div className="events-list">
 					{this.props.eventData.events.filter(theEvent => theEvent.hidden !== true).map((theEvent, index) => (
 						<EventRow key={index} event={theEvent} />
