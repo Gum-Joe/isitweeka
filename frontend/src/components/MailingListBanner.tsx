4d5e3d1f51818e76e559be690efe4867b2ec6a79
Madeline Hart <madeline@rykantech.com>
2021-01-21 21:34:11 +0000

    tweak styling (make light)

--- frontend/src/components/MailingListBanner.tsx
+++ frontend/src/components/MailingListBanner.tsx
@@ -8,7 +8,7 @@ export default class Banner extends React.Component {
 
 	render() {
 		return (
-			<div style={{ display: "flex", position: "sticky", width: "100vw", height: 80, padding: "0 5vw", margin: 0, top: 0, bottom: 0, justifyContent: "center", alignItems: "center", gap: 16, boxSizing: "border-box", background: "#000" }}>
+			<div style={{ display: "flex", position: "sticky", width: "100vw", height: 80, padding: "0 5vw", margin: 0, top: 0, bottom: 0, justifyContent: "center", alignItems: "center", gap: 16, boxSizing: "border-box", color: "#000", background: "#FFF" }}>
 				<h3 style={{ margin: "auto 0", fontSize: "2em" }}>We have a mailing list!</h3>
 				<form style={{ display: "flex", flexDirection: "row", gap: 16, margin: "auto 0", fontWeight: 500, fontSize: "1em" }}>
 					<div style={{ display: "flex", flexDirection: "row", gap: 16 }}>
@@ -19,11 +19,11 @@ export default class Banner extends React.Component {
 						<label style={{ margin: "auto 0" }} htmlFor="email-input">Email:</label>
 						<input style={{ margin: "auto 0" }} id="email-input" type="email" />
 					</div>
-					<div style={{ display: "flex", flexDirection: "row", gap: 16 }}>
+					<div style={{ display: "flex", flexDirection: "row", gap: 4 }}>
 						<label style={{ margin: "auto 0" }} htmlFor="dob-input">I am in year 9 or above:</label>
 						<input style={{ margin: "auto 0" }} id="dob-input" type="checkbox" />
 					</div>
-					<Button type="submit" onClick={this.handleClick}>Sign up</Button>
+					<Button type="submit" onClick={this.handleClick} light>Sign up</Button>
 				</form>
 			</div>
 		)
