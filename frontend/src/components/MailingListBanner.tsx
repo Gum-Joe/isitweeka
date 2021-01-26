1c4fdd6d817270aba52d2f367262a625c8eef4f7
Kishan <kishansambhi@hotmail.co.uk>
2021-01-26 15:12:40 +0000

    Move styles foir mailing list to App.css

--- frontend/src/components/MailingListBanner.tsx
+++ frontend/src/components/MailingListBanner.tsx
@@ -8,20 +8,20 @@ export default class Banner extends React.Component {
 
 	render() {
 		return (
-			<div className={"sosumi"} style={{ display: "flex", position: "sticky", width: "100vw", height: 80, padding: "0 5vw", margin: 0, top: 0, bottom: 0, justifyContent: "center", alignItems: "center", gap: 16, boxSizing: "border-box", color: "#000", background: "#FFF" }}>
-				<h3 style={{ margin: "auto 0", fontSize: "2em" }}>We have a mailing list!</h3>
-				<form style={{ display: "flex", flexDirection: "row", gap: 16, margin: "auto 0", fontWeight: 500, fontSize: "1em" }}>
-					<div style={{ display: "flex", flexDirection: "row", gap: 16 }}>
-						<label style={{ margin: "auto 0" }} htmlFor="name-input">Name:</label>
-						<input style={{ margin: 0 }} id="name-input" type="text" />
+			<div className={"sosumi"}>
+				<h3>We have a mailing list!</h3>
+				<form>
+					<div className="input-container">
+						<label htmlFor="name-input">Name:</label>
+						<input id="name-input" type="text" />
 					</div>
-					<div style={{ display: "flex", flexDirection: "row", gap: 16 }}>
-						<label style={{ margin: "auto 0" }} htmlFor="email-input">Email:</label>
-						<input style={{ margin: 0 }} id="email-input" type="email" />
+					<div className="input-container">
+						<label htmlFor="email-input">Email:</label>
+						<input id="email-input" type="email" />
 					</div>
-					<div style={{ display: "flex", flexDirection: "row", gap: 4 }}>
-						<label style={{ margin: "auto 0" }} htmlFor="dob-input">I am in year 9 or above:</label>
-						<input style={{ margin: "auto 0" }} id="dob-input" type="checkbox" />
+					<div className="input-checkbox-container">
+						<label htmlFor="dob-input">I am in year 9 or above:</label>
+						<input id="dob-input" type="checkbox" />
 					</div>
 					<Button style={{ fontSize: "1em" }} type="submit" onClick={this.handleClick} light>Sign up</Button>
 				</form>
