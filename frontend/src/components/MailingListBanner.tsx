d4b725f3828ef58dec9a27dfe1111904167d045f
Kishan <kishansambhi@hotmail.co.uk>
2021-01-26 16:34:14 +0000

    Add mailchimp integration & tracking

--- frontend/src/components/MailingListBanner.tsx
+++ frontend/src/components/MailingListBanner.tsx
@@ -4,20 +4,30 @@ import Button from "./Button.Forward";
 export default class Banner extends React.Component {
 	handleClick = () => {
 		// Here you are Kishan!
+		// Kishan: xD
+
+		/** Track sign ups */
+		gtag("event", "maillist_signup", {
+			"event_category": "engagement",
+			"value": "true",
+			"label": "maillist_signup",
+		});
 	}
 
 	render() {
 		return (
 			<div className={"sosumi"}>
 				<h3>We have a mailing list!</h3>
-				<form>
+				<form action="https://isitweeka.us7.list-manage.com/subscribe/post" method="POST">
+					<input type="hidden" name="u" value="1a205026e7a571c5b62dd369d" />
+					<input type="hidden" name="id" value="249833b0f4" />
 					<div className="input-container">
 						<label htmlFor="name-input">Name:</label>
-						<input id="name-input" type="text" />
+						<input id="name-input MERGE6" type="text" name="MERGE6" />
 					</div>
 					<div className="input-container">
 						<label htmlFor="email-input">Email:</label>
-						<input id="email-input" type="email" />
+						<input id="email-input MERGE0" type="email" name="MERGE0" />
 					</div>
 					<div className="input-checkbox-container">
 						<label htmlFor="dob-input">I am in year 9 or above:</label>
