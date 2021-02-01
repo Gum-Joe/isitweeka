7a01ed18d21b2e3fdd3b430af4c9435e166c4a04
Kishan <kishansambhi@hotmail.co.uk>
2021-02-01 18:25:03 +0000

    Add base modal Add base modal Add styles (mostly copied) for the new modal

--- frontend/src/components/MailingListBanner.tsx
+++ frontend/src/components/MailingListBanner.tsx
@@ -38,7 +38,27 @@ export default class Banner extends React.Component {
 					<Button buttonType="fill" style={{ fontSize: "1em" }} type="submit" onClick={this.handleClick} light>Sign up</Button>
 				</form>
 				<Portal>
-					
+					<div className="sosumi-mobile">
+						<h3>Want updates on the exciting things we have in store?</h3>
+						<form action="https://isitweeka.us7.list-manage.com/subscribe/post" method="POST">
+							{/* NOTE: DO NOT DELETE THESE HIDDEN VALUES OR CHANGE THE SUBMIT LINK OR THINGS WILL BREAK */}
+							<input type="hidden" name="u" value="1a205026e7a571c5b62dd369d" />
+							<input type="hidden" name="id" value="249833b0f4" />
+							<div className="input-container">
+								<label htmlFor="name-input">Name:</label>
+								<input id="name-input MERGE6" type="text" name="MERGE6" />
+							</div>
+							<div className="input-container">
+								<label htmlFor="email-input">Email:</label>
+								<input id="email-input MERGE0" type="email" name="MERGE0" />
+							</div>
+							<div className="input-checkbox-container">
+								<label htmlFor="dob-input">I am in year 9 or above:</label>
+								<input id="dob-input" type="checkbox" />
+							</div>
+							<Button buttonType="fill" style={{ fontSize: "1em" }} type="submit" onClick={this.handleClick} light>Sign up</Button>
+						</form>
+					</div>
 				</Portal>
 			</div>
 		);
