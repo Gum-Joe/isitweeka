f9d86c1996d21960c7397291422362cb29a63a1a
Kishan <kishansambhi@hotmail.co.uk>
2021-02-03 22:06:06 +0000

    Add layout for credits

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -12,7 +12,10 @@ const Footer: React.FC = () => {
 		<div className="footer">
 			<a id="feedback-buttom" href="#"><Button >give feedback</Button></a>
 
-			<p>Created by Madeline Hart & Kishan Sambhi.<br />&#169; {(new Date()).getFullYear()}, source available on GitHub under the MIT license.</p>
+			<p>
+				Created by Kishan Sambhi &amp; Madeline Hart.<br />
+				&#169; {(new Date()).getFullYear()}, source available on GitHub under the MIT license.
+			</p>
 
 			<div className="outbound-links">
 				<a target="__blank" href="https://github.com/Gum-Joe/isitweeka">
@@ -29,16 +32,27 @@ const Footer: React.FC = () => {
 				<a target="__blank" href="/privacy.html"><u>Privacy Policy</u></a>
 			</div>
 
-			<h4>Find Kishan here:</h4>
-			<ul>
-				<a target="__blank" href="https://github.com/Gum-Joe"><li><FontAwesomeIcon icon={faGithub} /> GitHub</li></a>
-				<a target="__blank" href="https://twitter.com/k_sam_mighty"><li><FontAwesomeIcon icon={faTwitter} /> Twitter</li></a>
-				<a target="__blank" href="https://instagram.com/k_sam_mighty"><li><FontAwesomeIcon icon={faInstagram} /> Instagram</li></a>
-				{/* NOTE: Change to a personal e-mail when leaving school */}
-				<a target="__blank" href="mailto:15Sambhi614@camphillboys.bham.sch.uk"><li><FontAwesomeIcon icon={faEnvelope} /> Email</li></a>
-			</ul>
-			<h4>Find Madaline here:</h4>
-			<p></p>
+			<div className="container-credits">
+				<div>
+					<h4>Find Kishan here:</h4>
+					<ul>
+						<a target="__blank" href="https://github.com/Gum-Joe"><li><FontAwesomeIcon icon={faGithub} /> GitHub</li></a>
+						<a target="__blank" href="https://twitter.com/k_sam_mighty"><li><FontAwesomeIcon icon={faTwitter} /> Twitter</li></a>
+						<a target="__blank" href="https://instagram.com/k_sam_mighty"><li><FontAwesomeIcon icon={faInstagram} /> Instagram</li></a>
+						{/* NOTE: Change to a personal e-mail when leaving school */}
+						<a target="__blank" href="mailto:15Sambhi614@camphillboys.bham.sch.uk"><li><FontAwesomeIcon icon={faEnvelope} /> Email</li></a>
+					</ul>
+				</div>
+
+				<div>
+					<h4>Find Madeline here:</h4>
+					<ul>
+						<a target="__blank" href="https://github.com/ILikeTeaALot"><li><FontAwesomeIcon icon={faGithub} /> GitHub</li></a>
+						<a target="__blank" href="https://rykan.net"><li>rykan.net</li></a>
+					</ul>
+				</div>			
+			</div>
+		
 		</div>
 	);
 };
