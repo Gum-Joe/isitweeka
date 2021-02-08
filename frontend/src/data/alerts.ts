17f6091a902860d2c5c865fccc1517bbf67abf90
Kishan <kishansambhi@hotmail.co.uk>
2021-02-08 11:20:36 +0000

    Update alerts.ts

--- frontend/src/data/alerts.ts
+++ frontend/src/data/alerts.ts
@@ -4,8 +4,6 @@ export const KECHBAlerts: AlertResponce = {
 	showAlert: true,
 	//message: "BREAKING: Student Council Chair Adeen Irfan has resigned, triggering an election.",
 	// alertLevel: ThreatLevels.CRITICAL,
-	message: "Welcome to IsItWeekA! This site is currently in beta & will be publicly launched first week back.",
+	message: "Welcome to IsItWeekA - it's is currently in beta & will be publicly launched first week back. Make sure to follow our socials!",
 	alertLevel: ThreatLevels.INFO,
-	linkTo: "#",
-	linkText: "More Info"
 };
