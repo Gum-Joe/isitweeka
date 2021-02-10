521985f645a3eeacb6f7acaefa7b05dc5876cdd6
Kishan <kishansambhi@hotmail.co.uk>
2021-02-10 09:20:28 +0000

    Change alert

--- frontend/src/data/alerts.ts
+++ frontend/src/data/alerts.ts
@@ -1,9 +1,19 @@
 import { AlertResponce, ThreatLevels } from "../utils/AlertInterfaces";
 
-export const KECHBAlerts: AlertResponce = {
+/*export const KECHBAlerts: AlertResponce = {
 	showAlert: true,
 	//message: "BREAKING: Student Council Chair Adeen Irfan has resigned, triggering an election.",
 	// alertLevel: ThreatLevels.CRITICAL,
 	message: "Welcome to IsItWeekA - it's currently in beta & will be fully launched first week back. Make sure to follow our socials!",
 	alertLevel: ThreatLevels.INFO,
+};*/
+
+export const KECHBAlerts: AlertResponce = {
+	showAlert: true,
+	//message: "BREAKING: Student Council Chair Adeen Irfan has resigned, triggering an election.",
+	// alertLevel: ThreatLevels.CRITICAL,
+	message: "NEWS: KECHB team \"The Standard Solutions\" wins the Cambridge Chemistry Race, beating 48 other schools from across the country",
+	alertLevel: ThreatLevels.LOW,
+	linkText: "more info",
+	linkTo: "https://twitter.com/KEVICHB/status/1359153991104557064?s=20"
 };
