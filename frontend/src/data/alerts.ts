e37628470ccc973f460ec496fd3d0a4fe0b7cc6c
Kishan <kishansambhi@hotmail.co.uk>
2021-02-28 12:01:09 +0000

    Change alert

--- frontend/src/data/alerts.ts
+++ frontend/src/data/alerts.ts
@@ -12,8 +12,8 @@ export const KECHBAlerts: AlertResponce = {
 	showAlert: true,
 	//message: "BREAKING: Student Council Chair Adeen Irfan has resigned, triggering an election.",
 	// alertLevel: ThreatLevels.CRITICAL,
-	message: "NEWS: KECHB team \"The Standard Solutions\" wins the Cambridge Chemistry Race, beating 48 other schools from across the country",
-	alertLevel: ThreatLevels.LOW,
-	linkText: "more info",
-	linkTo: "https://twitter.com/KEVICHB/status/1359153991104557064?s=20"
+	message: "Lateral Flow Testing: Please keep an eye on your parent/guardian's emails. You should have been already been sent some information.",
+	alertLevel: ThreatLevels.SEVERE,
+	//linkText: "more info",
+	//linkTo: "https://twitter.com/KEVICHB/status/1359153991104557064?s=20"
 };
