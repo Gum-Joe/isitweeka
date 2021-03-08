9f0f40388ffff025f5d1010dbb358bfb60c2eb65
Kishan <kishansambhi@hotmail.co.uk>
2021-03-08 10:11:34 +0000

    Update alerts.ts

--- frontend/src/data/alerts.ts
+++ frontend/src/data/alerts.ts
@@ -12,8 +12,8 @@ export const KECHBAlerts: AlertResponce = {
 	showAlert: true,
 	//message: "BREAKING: Student Council Chair Adeen Irfan has resigned, triggering an election.",
 	// alertLevel: ThreatLevels.CRITICAL,
-	message: "Lateral Flow Testing: Please keep an eye on your parent/guardian's emails. You should have been already been sent some information.",
-	alertLevel: ThreatLevels.SEVERE,
+	message: "Welcome back to school everyone! We hope you've managed to make the most of lockdown.",
+	alertLevel: ThreatLevels.LOW,
 	//linkText: "more info",
 	//linkTo: "https://twitter.com/KEVICHB/status/1359153991104557064?s=20"
 };
