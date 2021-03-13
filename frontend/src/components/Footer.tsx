736d2575f4e2a35012e25d6f12449a55e0525b1d
Kishan <kishansambhi@hotmail.co.uk>
2021-03-13 20:54:30 +0000

    Remove word approve

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -18,7 +18,7 @@ const Footer: React.FC = () => {
 				Created by Kishan Sambhi &amp; Madeline Hart.<br />
 				&#169; {(new Date()).getFullYear()}, source available on GitHub under the MIT license.<br /><br />
 				Data controller information: data controlled by Kishan Sambhi, a KECH pupil, and will only ever be viewed by KECH pupils.<br />
-				All changes to this site are reviewed, approved, merged and deployed by a KECH pupil.<br /><br />
+				All changes to this site are reviewed, merged and deployed by a KECH pupil.<br /><br />
 				Version {version}
 			</p>
 
