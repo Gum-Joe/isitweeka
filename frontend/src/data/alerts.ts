daf9f8eab53615d34021a34255b21ec0532aa28d
Kishan <kishansambhi@hotmail.co.uk>
2021-10-06 23:10:24 +0100

    Update KECHG Alert

--- frontend/src/data/alerts.ts
+++ frontend/src/data/alerts.ts
@@ -16,15 +16,15 @@ export const KECHBAlerts: AlertResponce = {
 	alertLevel: ThreatLevels.INFO,
 	message: "HOWARD IN THE HOUSE: Howard takes the Senior Football title, as Beaufort snatches second place from Tudor.",
 	linkText: "see more",
-	linkTo: "https://www.instagram.com/p/CUp1nGPIqLr/?utm_source=ig_web_copy_link"
+	linkTo: "https://www.instagram.com/p/CUp1nGPIqLr"
 };
 
 export const KECHGAlerts: AlertResponce = {
 	showAlert: true,
 	//message: "BREAKING: Student Council Chair Adeen Irfan has resigned, triggering an election.",
 	// alertLevel: ThreatLevels.CRITICAL,
-	message: "Welcome to IsItWeekA! We'll use these banners for important alerts in the future.",
+	message: "KECHG raises £1160.26 from a doughnut sale for the Macmillan Coffee Morning, selling 490 doughnuts in 5 minutes!",
 	alertLevel: ThreatLevels.LOW,
-	//linkText: "more info",
-	//linkTo: "https://twitter.com/KEVICHB/status/1359153991104557064?s=20"
+	linkText: "see more",
+	linkTo: "https://www.instagram.com/p/CUsfdZwIZ41"
 };
