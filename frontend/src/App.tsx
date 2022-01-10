9040e9ff7e87f1c3d361b995243ce89f868bfe95
James Hart <james.s.hart@outlook.com>
2022-01-10 23:15:27 +0000

    fix Linting

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -124,8 +124,7 @@ class App extends Component<Record<string, never>> {
 							() => { gaSetState(true); window.location.reload(); }
 						}
 						style={this.hideCookieConsent ? undefined : { display: "none" }}
-						// Due to the version of react-cookie-consent used missing the type declaration for the "visible" prop, this hack is sadly required.
-						/// @ts-expect-error
+						/// @ts-expect-error Due to the version of react-cookie-consent used missing the type declaration for the "visible" prop, this hack is sadly required.
 						visible={this.hideCookieConsent ? "hidden" : "byCookieValue"}
 					>
 						This website uses cookies for preferences and analytics (via Google Analytics).
