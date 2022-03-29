4f440a83b121cfe3f18117a1534112d01be36d4f
Madeline Hart <madeline@rykan.net>
2022-03-29 18:29:42 +0000

    add missing space

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -17,7 +17,7 @@ const Footer: React.FC = () => {
 			<p>
 				Created by Kishan Sambhi &amp; Madeline Hart. <br />
 				&#169; {(new Date()).getFullYear()}, source available on GitHub under the MIT license.<br /><br />
-				Data controller information: data controlled by Kishan Sambhi,a KECH pupil, and will only ever be viewed by KECH pupils.<br />
+				Data controller information: data controlled by Kishan Sambhi, a KECH pupil, and will only ever be viewed by KECH pupils.<br />
 				All changes to this site are reviewed, merged and deployed by a KECH pupil.<br /><br />
 				Version {packageJSON.version}
 			</p>
