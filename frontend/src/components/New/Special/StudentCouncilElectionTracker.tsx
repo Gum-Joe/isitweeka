89c336693e9a2fa933e2aa21a15c197c8cc8a5a4
Madeline Hart <madeline@rykan.net>
2022-04-03 22:21:44 +0000

    fix election card on mobile

--- frontend/src/components/New/Special/StudentCouncilElectionTracker.tsx
+++ frontend/src/components/New/Special/StudentCouncilElectionTracker.tsx
@@ -34,27 +34,29 @@ interface TrackerProps {
 
 export const StudentCouncilElectionTracker: React.FunctionComponent<TrackerProps> = (props) => {
 	return (
-		<div className="card light">
-			<div className="panel title" style={{ gap: 0 }}>
-				<div className="text big">Student Council Election</div>
-				<div className="text body" style={{ textTransform: "uppercase" }}>Councillor Opinion Poll</div>
-			</div>
-			<div className="panel">
-				<div className="special-candidate-info-container text big first">
-					<div className="special-candidate-photo"><img src="https://placekitten.com/120/120" alt={props.candidateOne.name} /></div>
-					<div className="special-candidate-name">{props.candidateOne.name}</div>
+		<div className="special-election-tracker-container">
+			<div className="card light">
+				<div className="panel title" style={{ gap: 0 }}>
+					<div className="text big">Student Council Election</div>
+					<div className="text body" style={{ textTransform: "uppercase" }}>Councillor Opinion Poll</div>
 				</div>
-				<div className="text body">{props.candidateOne.votes} Votes · {(props.candidateOne.votes / props.total * 100).toFixed(1)}%</div>
-				<ComparisonBar total={props.total} values={[props.candidateOne.votes, props.candidateTwo.votes]} colours={[props.candidateOne.colour, props.candidateTwo.colour]} />
-				<div className="text body" style={{ marginLeft: "auto", textAlign: "right" }}>{props.candidateTwo.votes} Votes · {(props.candidateOne.votes / props.total * 100).toFixed(1)}%</div>
-				<div className="special-candidate-info-container text big second">
-					<div className="special-candidate-name">{props.candidateTwo.name}</div>
-					<div className="special-candidate-photo"><img src="https://placekitten.com/120/120" alt={props.candidateTwo.name} /></div>
+				<div className="panel">
+					<div className="special-candidate-info-container text big first">
+						<div className="special-candidate-photo"><img src="https://placekitten.com/120/120" alt={props.candidateOne.name} /></div>
+						<div className="special-candidate-name">{props.candidateOne.name}</div>
+					</div>
+					<div className="text body">{props.candidateOne.votes} Votes · {(props.candidateOne.votes / props.total * 100).toFixed(1)}%</div>
+					<ComparisonBar total={props.total} values={[props.candidateOne.votes, props.candidateTwo.votes]} colours={[props.candidateOne.colour, props.candidateTwo.colour]} />
+					<div className="text body" style={{ marginLeft: "auto", textAlign: "right" }}>{props.candidateTwo.votes} Votes · {(props.candidateTwo.votes / props.total * 100).toFixed(1)}%</div>
+					<div className="special-candidate-info-container text big second">
+						<div className="special-candidate-name">{props.candidateTwo.name}</div>
+						<div className="special-candidate-photo"><img src="https://placekitten.com/120/120" alt={props.candidateTwo.name} /></div>
+					</div>
 				</div>
-			</div>
-			<div className="panel description" style={{ gap: 0 }}>
-				<div className="text body">Projection:</div>
-				<div className="text big">{props.summary}</div>
+				<div className="panel description" style={{ gap: 0 }}>
+					<div className="text body">Projection:</div>
+					<div className="text big">{props.summary}</div>
+				</div>
 			</div>
 		</div>
 	)
