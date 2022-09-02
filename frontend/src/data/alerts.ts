be513e0851599055fdede6495aea29f29d7e8cba
Kishan <kishansambhi@hotmail.co.uk>
2022-09-02 11:12:11 +0100

    Update alerts.ts

--- frontend/src/data/alerts.ts
+++ frontend/src/data/alerts.ts
@@ -22,9 +22,9 @@ import { AlertResponce, ThreatLevels } from "../utils/AlertInterfaces";
 export const KECHBAlerts: AlertResponce = {
 	showAlert: true,
 	alertLevel: ThreatLevels.INFO,
-	message: "Donate now to the Walk to Ukraine fundraiser",
-	linkText: "Donate Now  →",
-	linkTo: "https://www.justgiving.com/fundraising/chb-charity-committee?utm_medium=qrcode&utm_source=offline&utm_term=d00419302"
+	message: "Welcome back everyone!",
+	//linkText: "Donate Now  →",
+	//linkTo: "https://www.justgiving.com/fundraising/chb-charity-committee?utm_medium=qrcode&utm_source=offline&utm_term=d00419302"
 };
 
 export const KECHGAlerts: AlertResponce = {
